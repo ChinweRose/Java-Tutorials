1ba49ec3052be9016ad9b5fcda263a1dfde6f6f8
Chinwendu <chinwendurosemary30@gmail.com>
2022-12-10 12:42:05 +0100

    Company

---
+++ src/Company.java
@@ -0,0 +1,30 @@
+public class Company {
+
+    private String name = "Chroma";
+    private int age = 10;
+    private int amount = 10000;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
